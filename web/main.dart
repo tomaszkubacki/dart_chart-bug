1e1a3fb3fd8e8436e0fba29af7a8556de0cf5bf1
Tomasz Kubacki <tomasz.kubacki@moocode.net>
2016-11-19 16:20:52 +0100

    fix dart2js getting pie item index, change color on click

--- web/main.dart
+++ web/main.dart
@@ -3,48 +3,58 @@
 
 import 'dart:html';
 import 'dart:math' as math;
-import 'dart:js' as js;
 import 'package:chartjs/chartjs.dart';
 import 'package:js/js.dart';
+import 'package:js/js_util.dart' as js_util;
 
 
 void main() {
   var ctx = (querySelector('#canvas') as CanvasElement).context2D;
   var rnd = new math.Random();
   var months = <String>["January", "February", "March", "April", "May", "June"];
+  List dataVals = months.map((_) => rnd.nextInt(100)).toList();
+
+  List colors = [
+    "#2196F3",
+    "#F44336",
+    "#3F51B5",
+    "#009688",
+    "#E91E63",
+    "#673AB7",
+    "#FF9800",
+    "#8BC34A",
+    "#607D8B"
+  ];
+
+  Chart chart;
 
   var config = new ChartConfiguration(
       type: 'pie', data:
   new LinearChartData(labels: months, datasets: <ChartDataSets>[
     new ChartDataSets(
         label: "",
-        backgroundColor: [
-          "#2196F3",
-          "#F44336",
-          "#673AB7",
-          "#009688",
-          "#E91E63",
-          "#673AB7",
-          "#FF9800",
-          "#8BC34A",
-          "#607D8B",
-          "#3F51B5"
-        ],
-        data: months.map((_) => rnd.nextInt(100)).toList())
+        backgroundColor: colors,
+        data: dataVals)
   ])
   ,
-      options: new ChartOptions(responsive:false, onClick: js.allowInteropCaptureThis(([t,a,b]){
-    if(b != null && b is List && b.length > 0){
-      js.JsObject obj = new js.JsObject.fromBrowserObject(b[0]);
-      int index = obj["_index"];
-      print(index); ///!!!BUG - this is always null on dartjs and correct in dart/dartium
-    }
+      options: new ChartOptions(responsive:false, onClick: allowInteropCaptureThis(([t,a,b]){
+        if(b != null && b is List && b.length > 0){
+          var obj = b[0];
+          int index = js_util.getProperty(obj, "_index");
+          List newColors = colors.map((c)=> colors.indexOf(c) == index ? "#ffff00" : c ).toList(growable: false);
+          chart.config.data = new LinearChartData(labels: months, datasets: <ChartDataSets>[
+            new ChartDataSets(
+                label: "",
+                backgroundColor: newColors,
+                data: dataVals)
+          ]);
+
+          chart.update({"duration": 0});
+        }
   }) ) );
 
-  var chart = new Chart(ctx, config);
+  chart = new Chart(ctx, config);
 
-
-  chart.update();
 
 
 }
